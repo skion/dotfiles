deb80885aeb83158f57f35ca02c1470d95c725c6
Pieter Ennes <pieter.ennes@energyworx.org>
2019-10-10 21:48:12 +0100

    Add Fish shell.

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+eval (python3 -m virtualfish)
+
+set EDITOR vim
+set PATH $PATH ~/.local/bin
+
+alias grep "grep --color=auto"
+alias rgrep "grep --color=auto -HnR"
+alias jsonpp "python -m json.tool"
+alias httpify "python -m SimpleHTTPServer"
